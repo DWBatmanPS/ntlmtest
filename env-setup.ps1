08b0e8aa2efaa8f4feb5436cd07994f8f06dee2b
Dan Wheeler <96199880+DWBatmanPS@users.noreply.github.com>
2025-12-18 14:02:30 -0600

    Add PowerShell script to install NodeJS and Git

---
+++ env-setup.ps1
@@ -0,0 +1,17 @@
+
+# Define the download URL and the destination
+$nodeJsUrl = "https://nodejs.org/dist/v24.12.0/node-v24.12.0-x64.msi"
+$destination = "$env:TEMP\nodejs_installer.msi"
+
+# Download NodeJS installer
+Invoke-WebRequest -Uri $nodeJsUrl -OutFile $destination
+
+# Install NodeJS silently
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $env:TEMP\nodejs_installer.msi /qn" -Wait
+
+$githubInstallerUrl = "https://github.com/git-for-windows/git/releases/download/v2.52.0.windows.1/Git-2.52.0-64-bit.exe"
+$destinationGit = "$env:TEMP\GitInstaller.exe"
+
+Invoke-WebRequest -Uri $githubInstallerUrl -OutFile $destinationGit
+
+Start-Process -FilePath "GitInstaller.exe" -ArgumentList "/VERYSILENT", "/NORESTART", "$env:TEMP\GitInstaller.exe -Wait
